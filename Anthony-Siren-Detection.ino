e718c108e65003672af76eb85a968948b8dd7c63
pavith-g <pavith.gunasekara@gmail.com>
2022-06-28 13:43:11 +1000

    Use 2 LEDs for Relay Up and Relay Down

--- Anthony-Siren-Detection.ino
+++ Anthony-Siren-Detection.ino
@@ -7,114 +7,70 @@
  * Website: www.circuitdigest.com&nbsp;
 
  */
+#include <FreqMeasure.h>  //https://github.com/PaulStoffregen/FreqMeasure
 
-
-
-#include <FreqMeasure.h>//https://github.com/PaulStoffregen/FreqMeasure
+#define RELAY_UP_PIN 13
+#define RELAY_DOWN_PIN 12
 
 int last_triggered_time=0; //PG
-
-double sum=0; //Moved here by I.C
-
-int count=0; //Moved here by I.C
+double frequency_sum=0; //Moved here by I.C
+int frequency_count=0; //Moved here by I.C
+int frequency_continuity =0; //Moved here by I.C
+float frequency; //Moved here by I.C
 
 bool state = false; //Moved here by I.C
 
-float frequency; //Moved here by I.C
-
-int continuity =0; //Moved here by I.C
-
-
-
 
 void setup() {
-
   Serial.begin(9600);
-
   FreqMeasure.begin(); //Measures on pin 8 by default
-
-  pinMode(LED_BUILTIN, OUTPUT); //LED_BUILTIN is pin13 so same as RelayDown
-
-
+  pinMode(RELAY_UP_PIN, OUTPUT); 
+  pinMode(RELAY_DOWN_PIN, OUTPUT);
 }
 
-
 void loop() {
-
   if (FreqMeasure.available()) {
-
-    // average several reading together
-
-    sum = sum + FreqMeasure.read();
-
-    count = count + 1;
-
-    if (count > 100) {
-
-      frequency = FreqMeasure.countToFrequency(sum / count);
-
+    
+    // Calculate the average frequency over 100 loops
+    frequency_sum += FreqMeasure.read();
+    frequency_count += 1;
+    
+    if (frequency_count > 100) {
+      frequency = FreqMeasure.countToFrequency(frequency_sum / frequency_count);
       Serial.println(frequency);
-
-      sum = 0;
-
-      count = 0;
-
+      frequency_sum = 0;
+      frequency_count = 0;
     }
-
   }
 
+  if (frequency>2400 && frequency<3300) {
+    // IC's guess at siren frequency. Possibly use a long response to rule out car horns.
+    frequency_continuity++;
+    Serial.print("frequency_continuity -> ");
+    Serial.println(frequency_continuity);
+    frequency=0;
+  }
 
-
-    if (frequency>2400 && frequency<2700) // IC's guess at siren frequency. Possibly use a long response to rule out car horns.
-
-        { continuity++; Serial.print("Continuity -> "); Serial.println(continuity); frequency=0;}
-
-
-     // PG
-    if (continuity >= 3){ // We have detected a siren
-
-        // If we hear a siren for more than 3 counts (CONTINUITY), then we turn LED ON
-        // THEN wait 15 seconds regardless of sensors.
-        state = true;
+  // PG
+  if (frequency_continuity >= 3){ // We have detected a siren
+        // RELAY_UP = true
+        // RELAY_DOWN = false
         Serial.println("LIGHT TURNED ON ");
-        digitalWrite(LED_BUILTIN, state);
-        // We have heard a siren. The LED is turned on. Now wait 15 seconds.
+        // Pistons down
+        digitalWrite(RELAY_UP_PIN, LOW);
+        digitalWrite(RELAY_DOWN_PIN, HIGH);
+        
+        // We have heard a siren. RELAY_DOWN is HIGH. Now wait 15 seconds for the pistons to come down
         delay(15000);
-        // Reset the continuity to 0 -> reset the counter and recheck for frequencies.
-        continuity = 0;
 
-        // Turn off LED and wait "5" minutes
-        state = false;
-        digitalWrite(LED_BUILTIN, state);
+        // Now wait "5 Minutes" while pistons are down
         delay(300000);
+        // Reset the frequency_continuity to 0 -> reset the counter and recheck for frequencies.
+        frequency_continuity = 0;
+
+        // Set RELAY_UP to low and RELAY_DOWN to high to put pistons back up
+        digitalWrite(RELAY_UP_PIN, HIGH);
+        digitalWrite(RELAY_DOWN_PIN, LOW);
+        
     }
-    else // No siren detected
-    {
-      state = false;
-      digitalWrite(LED_BUILTIN, state);
-    }
-
-  /* IC
-    if (continuity >=3 && state==false)
-
-
-      {if (millis()- last_triggered_time >= 1000){
-       state = true;
-        continuity=0;
-        Serial.println("Light Turned ON");
-        //delay(1000);
-        last_triggered_time = millis();}}
-
-
-    if (continuity >=3 && state==true)
-
-       {if (millis()- last_triggered_time >= 15000){
-        state = false;
-         continuity=0;
-        Serial.println("Light Turned OFF");
-        //delay(1000);
-        last_triggered_time = millis();}}
-*/
-
-
 }
