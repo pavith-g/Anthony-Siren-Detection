509648cdf3fd23cac6927677e2b1d469ba1e3721
pavith-g <pavith.gunasekara1@gmail.com>
2022-06-29 13:40:24 +1000

    4 digit display now functioning alongside frequency detection

--- Anthony-Siren-Detection.ino
+++ Anthony-Siren-Detection.ino
@@ -7,30 +7,286 @@
  * Website: www.circuitdigest.com&nbsp;
 
  */
-#include <FreqMeasure.h>  //https://github.com/PaulStoffregen/FreqMeasure
+#include <FreqMeasure.h> //https://github.com/PaulStoffregen/FreqMeasure
 
 #define RELAY_UP_PIN 13
 #define RELAY_DOWN_PIN 12
 
-int last_triggered_time=0; //PG
-double frequency_sum=0; //Moved here by I.C
-int frequency_count=0; //Moved here by I.C
-int frequency_continuity =0; //Moved here by I.C
-float frequency; //Moved here by I.C
+// These pins refer to which digit on the display to update
+#define D1 2
+#define D2 3
+#define D3 4
+#define D4 5
 
-bool state = false; //Moved here by I.C
+// These pins refer to which segment to activate
+#define A 6
+#define B 7
+#define C 9
+#define D 10
+#define E 11
+#define F A0
+#define G A1
+#define P A2
+
+int last_triggered_time=0;
+double frequency_sum=0;
+int frequency_count=0;
+int frequency_continuity =0;
+float frequency;
+
+
+int del = 3; // Delay between updating display (ms)
+int value = 2345; // This is the value that will be printed onto the display - The maxmimum value is 9999
+int digits[] = {0, 0, 0, 0};
+
+// --------- 4D7S Functions --------- //
+
+void pickDigit(int x) { 
+  // Pick which digit we want to update
+  // Note that setting the digit high, means that it is not selected. Setting it low, means we select it. 
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, HIGH);
+  switch (x)
+  {
+    case 0:
+      digitalWrite(D1, LOW);//Light d1 up
+      break;
+    case 1:
+      digitalWrite(D2, LOW); //Light d2 up
+      break;
+    case 2:
+      digitalWrite(D3, LOW); //Light d3 up
+      break;
+    default:
+      digitalWrite(D4, LOW); //Light d4 up
+      break;
+  }
+}
+
+void clearLEDs() //clear the 7-segment display screen
+{
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(P, LOW);
+}
+
+// --- Different combinations of segments for each number 0 - 9 ---- // 
+void zero() {
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, LOW);
+  digitalWrite(P, LOW);
+}
+
+void one() {
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(P, LOW);
+}
+
+void two()
+{
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
+  digitalWrite(P, LOW);
+}
+void three() 
+{
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
+}
+void four() 
+{
+  digitalWrite(A, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+}
+
+void five()
+{
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+}
+
+void six()
+{
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+}
+
+void seven()
+{
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+}
+
+void eight()
+{
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+}
+
+void nine()
+{
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+}
+
+void pickNumber(int x){
+  // Based on the digit we have, print it to the display
+
+  switch (x)
+  {
+    default:
+      zero();
+      break;
+    case 1:
+      one();
+      break;
+    case 2:
+      two();
+      break;
+    case 3:
+      three();
+      break;
+    case 4:
+      four();
+      break;
+    case 5:
+      five();
+      break;
+    case 6:
+      six();
+      break;
+    case 7:
+      seven();
+      break;
+    case 8:
+      eight();
+      break;
+    case 9:
+      nine();
+      break;
+  }
+}
 
 
 void setup() {
   Serial.begin(9600);
   FreqMeasure.begin(); //Measures on pin 8 by default
+
+  
+  // Set pin values on arduino
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(P, OUTPUT);
   pinMode(RELAY_UP_PIN, OUTPUT); 
   pinMode(RELAY_DOWN_PIN, OUTPUT);
 }
 
 void loop() {
+  // Updating the 4 DIGIT DISPLAY
+  value += 1;
+  
+  if (value > 9999){
+    value = 9999;
+    Serial.print("Value overflow");
+  }
+  
+  pickDigit(0);
+  clearLEDs();
+  pickNumber((value % 10000) / 1000); // Get the first digit and update
+  digits[0] = (value % 10000) / 1000;
+  delay(del);
+  pickDigit(1);
+  clearLEDs();
+  pickNumber((value % 1000) / 100); // Get the second digit and update
+  digits[1] = (value % 1000) / 100;
+  delay(del);
+
+  pickDigit(2);
+  clearLEDs();
+  pickNumber((value % 100) / 10); // Get the third digit and update
+  digits[2] = ((value % 100) / 10);
+  delay(del);
+
+  
+  pickDigit(3);
+  clearLEDs();
+  pickNumber((value % 10) / 1); // Get the fourth digit and update
+  digits[3] = ((value % 10) / 1);
+  delay(del);
+
+
+  Serial.print(digits[0]);Serial.print(digits[1]);Serial.print(digits[2]);Serial.println(digits[3]);
+  
+
+  
   if (FreqMeasure.available()) {
-    
     // Calculate the average frequency over 100 loops
     frequency_sum += FreqMeasure.read();
     frequency_count += 1;
@@ -42,7 +298,7 @@ void loop() {
       frequency_count = 0;
     }
   }
-
+  /*
   if (frequency>2400 && frequency<3300) {
     // IC's guess at siren frequency. Possibly use a long response to rule out car horns.
     frequency_continuity++;
@@ -51,7 +307,6 @@ void loop() {
     frequency=0;
   }
 
-  // PG
   if (frequency_continuity >= 3){ // We have detected a siren
         // RELAY_UP = true
         // RELAY_DOWN = false
@@ -71,6 +326,6 @@ void loop() {
         // Set RELAY_UP to low and RELAY_DOWN to high to put pistons back up
         digitalWrite(RELAY_UP_PIN, HIGH);
         digitalWrite(RELAY_DOWN_PIN, LOW);
-        
     }
+    */
 }
